2d7e93f518e7cc0c43a6a12bc9bdd55a985ab177
paulihs <1105312001@qq.com>
2020-09-18 09:09:02 +0800

    修改

--- supreme-project/src/index.js
+++
@@ -1,9 +0,0 @@
-// console.log(a);
-//
-// import a, {abs} from '../src/bye.js';
-// import * as ni from '../src/bye.js';
-import {abs} from '../src/bye.js';
-// console.log(abs())
-// console.log(ni)
-// a();
-abs();
